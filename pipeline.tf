ef03297ea47979cb39e92403dd57228b65e2b0ce
Jay Chauhan <PCE30@server7.com>
2023-08-09 15:36:52 +0530

    bfedff

--- pipeline.tf
+++ pipeline.tf
@@ -83,7 +83,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
 
       configuration = {
         ConnectionArn        = var.codestar_connector_credentials
-        FullRepositoryId     = "ci-cd23"
+        FullRepositoryId     = "davoclock/ci-cd23"
         BranchName           = "master"
         OutputArtifactFormat = "CODE_ZIP"
       }
